e34fbbc8f1aa4edf9c5de5268131a5685ddd6fdf
Deson Dai <1155211130@link.cuhk.edu.hk>
2025-03-31 13:59:51 +0800

    Update

--- Read_Results.py
+++ Read_Results.py
@@ -1,30 +1,56 @@
 import argparse
 import json
 import os
-import pickle
 
-def Read_results(test_meta, test_prefix, test_mode, test_name):
+dict ={
+    'CF1':5,
+    'CF2':6,
+    'CF3':0,
+    'CS1':0,
+    'CS2':0,
+    'CS3':0,
+    'I3':8,
+    'P3':5,
+    'S1':8,
+    'S2':6,
+    'SS2':5,
+    'SS3':10,
+    'VZ1':5,
+    'VZ2':5,
+    'VZ3':5
+}
+
+def calc(answer, num):
+    if num==0:
+        return answer[-1]*100/len(answer)
+    base = 0
+    count = 0
+    now = 0
+    for i in range(len(answer)):
+        if i%num == 0:
+            if now-base == num:
+                count += 1
+            base = now
+        now = answer[i]
+
+    return count*100/(len(answer)/num)
+
+def Read_results(test_meta, test_prefix, test_mode,test_id):
+    answer = []
+    num = dict[test_id]
     if test_mode == 'Split' and "Split" not in test_meta.keys():
         return
     for i, (q, a) in enumerate(zip(test_meta[test_mode]["Questions"], test_meta[test_mode]["Answers"])):
-        output_path_R = f"{test_prefix}_summary.txt"
         output_path = f"{test_prefix}-{i}.json"
         output_path = output_path.replace("Results_read","Results")
-        if(not os.path.exists(output_path)):
-            break
-        if i == 0:
-            with open(output_path_R, 'w') as file:
-                file.write('')
-        if os.path.isfile(output_path_R):
-            with open(f"{output_path[:-5]}.txt", 'r') as f:
-                all_count, gpt_correct, gpt_invalid = [int(_) for _ in f.read().splitlines()[2][5:].split(',')]
-            with open(f"{output_path[:-5]}", "rb") as f:
-                gpt_answers = pickle.load(f)
-            output = f"{i + 1},{gpt_correct},{gpt_invalid},\n"
-            print(output)
-            with open(f"{output_path_R}", 'a') as file:
-                file.write(output)
+        with open(f"{output_path[:-5]}.txt", 'r') as f:
+            all_count, gpt_correct, gpt_invalid = [int(_) for _ in f.read().splitlines()[2][5:].split(',')]
+        answer.append(gpt_correct)
+    # print(answer)
+    print(test_id,' ',calc(answer, num))
+    return
 
+Split = ['CF1', 'CF2', 'VZ2', 'VZ3', 'S1', 'SS3', 'P3', 'I3']
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -33,27 +59,21 @@ if __name__ == "__main__":
                         default="gpt4o-example-cot-group-0",
                         help="name of this run",
                         )
-    parser.add_argument("--mode",
-                        type=str,
-                        choices=['Split', 'Group'],
-                        default="Split",
-                        help="Split or Group",
-                        )
     opt = parser.parse_args()
     with open("meta.json", 'r') as f:
         data = json.load(f)
     for test_id, test in data.items():
-        test_prefix = f"Results_read/{test_id}-{test['Name']}"
+        if test_id not in Split:
+            test_prefix = f"Results_read/{test_id}-{test['Name']}"
 
-        if not os.path.isdir(test_prefix):
-            os.makedirs(test_prefix)
+            if not os.path.isdir(test_prefix):
+                os.makedirs(test_prefix)
 
-        test_prefix += f'/{opt.flag}'
+            test_prefix += f'/{opt.flag}'
 
-        Read_results(
-            test_meta=data[test_id],
-            test_prefix=test_prefix,
-            test_mode=opt.mode,
-            test_name=test['Name']
-        )
-
+            Read_results(
+                test_meta=data[test_id],
+                test_prefix=test_prefix,
+                test_mode='Group',
+                test_id=test_id
+            )
